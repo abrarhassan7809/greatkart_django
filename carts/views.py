884d7124b8f7d47797cbb54a7f02ce0f010fe56a
Ubuntu <abrarhassan7809@gmail.com>
2026-03-30 23:48:52 +0500

    add validation size/color

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 
 
 # Create your views here.
@@ -15,9 +15,22 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key,
+                                                  variation_value__iexact=value)
+                product_variation.append(variation)
+
+            except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
 
@@ -25,11 +38,21 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.quantity += 1
         cart_item.save()
 
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.save()
 
     return redirect('cart')
